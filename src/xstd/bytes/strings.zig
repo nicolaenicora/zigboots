5cda0b40e45931e3b580c5de57c50348a2ee9424
Nicolae Nicora <nicolae.nicora@taryafintech.com>
2023-09-23 14:46:48 +0200

    re-arrange the packages
    Signed-off-by: Nicolae Nicora <nicolae.nicora@taryafintech.com>

--- src/xstd/bytes/strings.zig
+++ src/xstd/bytes/strings.zig
@@ -1,6 +1,4 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-pub const rune = []const u8;
-pub const string = []const u8;
 pub const StringBuilder = @import("utf8_buffer.zig").Utf8Buffer(!builtin.single_threaded);
